828fc71de702dd04a26a430aeb9be7eca9efbbc0
azusa-kikuike <azusa.kikuike@gmail.com>
2013-01-30 23:40:55 +0900

    p37

--- function.hs
+++ function.hs
@@ -13,3 +13,12 @@ sayMe x = "Not between 1 and 5"
 factorial :: Int -> Int
 factorial 0 = 1
 factorial n = n * factorial(n - 1)
+
+charName :: Char -> String
+charName 'a' = "Albert"
+charName 'b' = "Broseph"
+charName 'c' = "Cecil"
+charName x = "Others"
+
+addVectors :: (Double, Double) -> (Double, Double) -> (Double, Double)
+addVectors (x1, y1) (x2, y2) = (x1 + x2, y1 + y2)
