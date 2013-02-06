4d7c88886137658fbece5a05b92f62e4e9d07dbc
azusa-kikuike <azusa.kikuike@gmail.com>
2013-02-06 21:52:42 +0900

    p49

--- function.hs
+++ function.hs
@@ -95,4 +95,9 @@ cylinder r h =
         topArea  = pi * r ^ 2
     in sideArea + 2 * topArea
 
+describeList :: [a] -> String
+describeList ls = "The list is "
+    ++ case ls of [] -> "empty."
+                  [x] -> "a singleton list."
+                  xs -> "a longer list."
 
