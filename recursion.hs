e17f29dc80dba732bc59b09ac1a1e3c4b319d3f3
azusa-kikuike <azusa.kikuike@gmail.com>
2013-02-11 21:56:42 +0900

    p54

--- recursion.hs
+++ recursion.hs
@@ -8,6 +8,10 @@ replicate' n x
   | n <= 0 = []
   | otherwise = x : replicate' (n-1) x
 
-
+take' :: Int -> [a] -> [a]
+take' n _
+  | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
 
 
